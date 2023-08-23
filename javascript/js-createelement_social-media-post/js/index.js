155facad59ac01d7ef8a800112b675dae15aaf20
CANSU CANER <CANANCANSUCANER@gmail.com>
2023-08-23 18:27:44 +0200

    social media post complete

--- javascript/js-createelement_social-media-post/js/index.js
+++ javascript/js-createelement_social-media-post/js/index.js
@@ -1,12 +1,53 @@
 console.clear();
+const body = document.querySelector('body');
+const likeButton = document.querySelector('[data-js="like-button"]');
 
 function handleLikeButtonClick(event) {
   const buttonElement = event.target;
   buttonElement.classList.toggle("post__button--liked");
 }
 
-const likeButton = document.querySelector('[data-js="like-button"]');
 likeButton.addEventListener("click", handleLikeButtonClick);
+
+const newPost = document.createElement('article');
+newPost.classList.add('post');
+
+const newPar = document.createElement('p');
+newPar.classList.add('post__content');
+newPar.textContent = 'bla bla bla thanks bla bla bla i guess. hu hu hu hu';
+
+const newFoot = document.createElement('footer');
+newFoot.classList.add('post__footer');
+
+const newButton = document.createElement('button');
+newButton.classList.add('post__button');
+newButton.textContent = '♥ Like';
+newButton.addEventListener("click", handleLikeButtonClick)
+
+const newSpan = document.createElement('span')
+newSpan.classList.add('post__username')
+newSpan.textContent = '@username' 
+
+newFoot.append(newSpan);
+newFoot.append(newButton);
+newPost.append(newPar);
+newPost.append(newFoot);
+body.append(newPost);
+
+
+
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+/* <article class="post">
+      <p class="post__content">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      </p>
+      <footer class="post__footer">
+        <span class="post__username">@username</span>
+        <button type="button" class="post__button" data-js="like-button">
+          ♥ Like
+        </button>
+      </footer>
+    </article> */
+
