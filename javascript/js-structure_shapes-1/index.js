fb2e214691281f28040457384166bab8f75f865d
CANSU CANER <CANANCANSUCANER@gmail.com>
2023-08-30 17:20:13 +0200

    rebase

---
+++ javascript/js-structure_shapes-1/index.js
@@ -0,0 +1,16 @@
+
+import {Circle} from "./components/Circle/Circle.js";
+import {Pentagon} from "./components/Pentagon/Pentagon.js";
+import {Square} from "./components/Square/Square.js";
+
+console.clear();
+
+const root = document.getElementById("root");
+
+const circle = Circle();
+const pentagon = Pentagon();
+const square = Square();
+
+
+
+root.append(circle, square, pentagon);
