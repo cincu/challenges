4f2a4e2f3134474d7de61569f7077b27f78fb503
CANSU CANER <CANANCANSUCANER@gmail.com>
2023-08-31 11:04:26 +0200

    incomplete

--- javascript/js-structure_quiz-app/index.js
+++ javascript/js-structure_quiz-app/index.js
@@ -1,37 +1,7 @@
 // Store the cards in a global state
-const cards = [
-  {
-    question:
-      "In the Kingdom Hearts series who provides the english voice for Master Eraqus?",
-    answer: "Mark Hamill",
-    tags: ["kingdomheart", "mastereraqus", "english voices"],
-    isBookmarked: false,
-  },
-  {
-    question:
-      "Daniel Radcliffe became a global star in the film industry due to his performance in which film franchise?",
-    answer: "Harry Potter",
-    tags: ["hewhomustnotbenamed", "danielradcliffe", "abracadabra"],
-    isBookmarked: false,
-  },
-  {
-    question:
-      'In the game series "The Legend of Zelda" what is the name of the main character?',
-    answer: "Link",
-    tags: ["linktothepast", "linkbetweenworlds", "linkbetweenages"],
-    isBookmarked: false,
-  },
-];
+import {cards} from "./components/Card/cards.js"
+import { Header } from "./components/Header/header.js";
 
-function Header() {
-  const header = document.createElement("header");
-  header.classList.add("header");
-  header.innerHTML = /* html */ `
-	  <h1 class="header__title">Quiz-App</h1>
-	`;
-
-  return header;
-}
 
 function handleFormSubmit(event) {
   event.preventDefault();
