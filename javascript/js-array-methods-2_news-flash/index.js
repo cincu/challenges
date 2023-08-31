0075a10ed3aa14f372a0ce4a2c7c92f0497ad171
CANSU CANER <CANANCANSUCANER@gmail.com>
2023-08-31 16:05:48 +0200

    news-flash challenge complete

--- javascript/js-array-methods-2_news-flash/index.js
+++ javascript/js-array-methods-2_news-flash/index.js
@@ -5,17 +5,34 @@ import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
 const container = document.querySelector('[data-js="card-container"]');
 
 // Part 1 - start here
-const filteredNews = news.filter(() => {
-  return true;
-});
+// ### Part 1: Filter
+
+// Have a look at the [`js/index.js`](./js/index.js) file: There is a variable
+//  `filteredNews` declared with `news.filter()` and a return value `true`.
+
+// You task is to change the filter method:
+
+// - filter all cards which have the category "politics" in their category array
+// - to do so, use the `includes` method on `card.categories`
+
+const filteredNews = news.filter((card) => card.categories.includes('politics'));
 
 // Part 2 - start here
 const sortedNews = filteredNews;
+
+sortedNews.sort((a, b) => {
+if (a.body.length < b.body.length) {
+  return -1;
+} if (a.body > b.body) {
+  return 1;
+}
+});
 
 sortedNews.forEach((news) => {
   const cardElement = Card(news);
   container.append(cardElement);
 });
+
 
 // Check your filter and sorting order here:
 checkFilteredNews(filteredNews);
