ea0a1a37b52ae13d7dc58bf5744f951926df9ea6
CANSU CANER <CANANCANSUCANER@gmail.com>
2023-09-04 11:27:24 +0200

    star-wars console challenge complete with bonus

--- javascript/js-fetch_star-wars-console/js/index.js
+++ javascript/js-fetch_star-wars-console/js/index.js
@@ -1,7 +1,27 @@
 console.clear();
+import { renderElement } from "./utils.js";
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+    try {
+        const response = await fetch(url)
+        console.log(response);
+
+        const jsonObj = await response.json();
+        console.log('jsonObj',jsonObj);
+        
+        if (!response.ok) {
+            console.log('bad request!');
+        } else {
+            console.log('fetch works!');
+            //bonus :
+            console.log(jsonObj.results[2])
+            return jsonObj;
+        } 
+    } catch (error) {
+        console.log(error.message)
+    }
+}
 
 fetchData();
