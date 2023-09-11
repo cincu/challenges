d8884d673fd05e3ccdc5f5c668e95877be4a71b8
CANSU CANER <CANANCANSUCANER@gmail.com>
2023-09-11 17:01:08 +0200

    button challenge complete

--- react/react-props_button/src/App.js
+++ react/react-props_button/src/App.js
@@ -2,5 +2,50 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  function handleClick() {
+    console.log("click click motherclicker!");
+  }
+  function handleNegClick() {
+    alert("i dare you to click again");
+  }
+
+  return (
+    <div>
+      <Button
+        id="bip"
+        color="pink"
+        disabled={false}
+        text="click me"
+        clicker={handleClick}
+      />
+      <Button
+        color="purple"
+        disabled={false}
+        text="don't click"
+        clicker={handleNegClick}
+      />
+    </div>
+  );
 }
+
+function Button({ color, disabled, text, clicker, id }) {
+  return (
+    <button
+      id={id}
+      disabled={disabled}
+      style={{ height: "100px", backgroundColor: color }}
+      onClick={clicker}
+    >
+      {text}
+    </button>
+  );
+}
+// let clickCount = 0;
+
+// document.addEventListener("click", (event) => {
+//   const ourButton = event.target.closest("#bip"); // event delegation
+//   if (event.target === ourButton) {
+//     console.log("increment", clickCount);
+//     clickCount += 1;
+//   }
+// });
