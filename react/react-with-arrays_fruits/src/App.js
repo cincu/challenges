8695624bf7b291461f099c4a4a0a7ba9758749a2
CANSU CANER <CANANCANSUCANER@gmail.com>
2023-09-13 16:18:16 +0200

    fruits challenge complete with bonus

--- react/react-with-arrays_fruits/src/App.js
+++ react/react-with-arrays_fruits/src/App.js
@@ -1,11 +1,19 @@
 import Card from "./components/Card";
 
-export default function App() {
-  const fruits = [];
+const fruits = [
+  { id: 1, name: "cherry", color: "darkred" },
+  { id: 2, name: "blueberry", color: "blue" },
+  { id: 3, name: "strawberry", color: "red" },
+  { id: 4, name: "mango", color: "orange" },
+  { id: 5, name: "coconut", color: "brown" },
+];
 
+export default function App() {
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map(({ id, name, color }) => (
+        <Card color={color} key={id} name={name} />
+      ))}
     </div>
   );
 }
