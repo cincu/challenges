d644af2293846967522884fed1ceabe8a676656f
CANSU CANER <CANANCANSUCANER@gmail.com>
2023-09-14 11:22:15 +0200

    change color number complete

--- react/react-state-2_colored-number/src/components/Counter.js
+++ react/react-state-2_colored-number/src/components/Counter.js
@@ -1,19 +1,23 @@
 import ColoredNumber from "./ColoredNumber";
+import { useState } from "react";
 
 export default function Counter() {
+  const [counterColor, setCounterColor] = useState(0);
   return (
     <div className="counter">
       <h1>Colored Number</h1>
-      <ColoredNumber value={0} />
+      <ColoredNumber value={counterColor} />
       <div className="counter__buttons-container">
         <button
           type="button"
           className="counter__button"
           aria-label="increment count"
+          onClick={() => setCounterColor(counterColor + 1)}
         >
           +
         </button>
         <button
+          onClick={() => setCounterColor(counterColor - 1)}
           type="button"
           className="counter__button"
           aria-label="decrement count"
