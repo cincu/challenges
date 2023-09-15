150650e1ef9a045dabe8b23cd20827b8a989bdcd
CANSU CANER <CANANCANSUCANER@gmail.com>
2023-09-15 12:22:15 +0200

    tags challenge compelte

--- react/react-state-3_tags/src/App.js
+++ react/react-state-3_tags/src/App.js
@@ -6,10 +6,16 @@ import List from "./components/List/index.js";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  function handleTag(newTag) {
+    setTags([...tags, newTag]);
+  }
+  function removeTag(tagToRemove) {
+    setTags(tags.filter((tag) => tag !== tagToRemove));
+  }
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleTag} />
+      <List tags={tags} onDeleteTag={removeTag} />
     </main>
   );
 }
