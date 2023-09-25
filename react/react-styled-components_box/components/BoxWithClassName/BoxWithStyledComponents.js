847350856926dd4e6cc2d2c34708bd8c81059774
CANSU CANER <CANANCANSUCANER@gmail.com>
2023-09-25 14:25:50 +0200

    blah

---
+++ react/react-styled-components_box/components/BoxWithClassName/BoxWithStyledComponents.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledBox = styled.div`
+  width: 100px;
+  height: 100px;
+  margin: 2rem;
+  background-color: ${({ isBlack }) => (isBlack ? "black" : "green")};
+  &:hover {
+    background-color: red;
+  }
+`;
+
+export default function BoxWithStyledComponents({ $isBlack }) {
+  return <StyledBox isBlack={$isBlack} />;
+}
--- react/react-styled-components_box/pages/index.js
+++ react/react-styled-components_box/pages/index.js
@@ -1,10 +1,15 @@
+import BoxWithStyledComponents from "@/components/BoxWithClassName/BoxWithStyledComponents.js";
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import styled from "styled-components";
 
 export default function HomePage() {
+  const Container = styled.div``;
   return (
     <div>
-      <BoxWithClassName />
-      <BoxWithClassName isBlack />
+      <BoxWithStyledComponents />
+      <BoxWithStyledComponents isBlack />
+      <BoxWithStyledComponents />
+      <BoxWithStyledComponents isBlack />
     </div>
   );
 }
