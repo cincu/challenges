ba0c1bb003146bdbef6ce8c5eeaac712867d0436
CANSU CANER <CANANCANSUCANER@gmail.com>
2023-10-09 14:08:14 +0200

    the main challenges are complete, bonus are in progress

--- backend/backend-read_products/pages/api/products/[id].js
+++ backend/backend-read_products/pages/api/products/[id].js
@@ -1,9 +1,11 @@
-import { products } from "../../../lib/products";
-
-export default function handler(request, response) {
+// import dbConnect from "@/db/dbConnect";
+import Product from "@/db/models/Product";
+export default async function handler(request, response) {
+  // await dbConnect();
   const { id } = request.query;
 
-  const product = products.find((product) => product.id === id);
+  // const product = await Product.findById(id);
+  const product = await Product.findById(id).populate("reviews");
 
   if (!product) {
     return response.status(404).json({ status: "Not Found" });
--- backend/backend-read_products/pages/api/products/index.js
+++ backend/backend-read_products/pages/api/products/index.js
@@ -1,5 +1,11 @@
-import { products } from "../../../lib/products";
-
-export default function handler(request, response) {
-  return response.status(200).json(products);
+import dbConnect from "@/db/dbConnect";
+import Product from "@/db/models/Product";
+export default async function handler(request, response) {
+  await dbConnect();
+  if (request.method === "GET") {
+    const products = await Product.find();
+    return response.status(200).json(products);
+  } else {
+    return response.status(404).json({ message: "method not allowed" });
+  }
 }
